6092643611558a880f5e64d51d56efb87d3b9d0a
Maltsev Vlad <shedwardx@gmail.com>
2013-09-13 02:07:18 +0700

    Add tests for Compiler.Compiler class.

--- timus/test/Compiler_test.py
+++ timus/test/Compiler_test.py
@@ -1,8 +1,10 @@
-from unittest import TestCase
+import unittest
+
+from os.path import abspath
 
 import timus.Compiler
 
-class TestSubstitude(TestCase):
+class TestSubstitude(unittest.TestCase):
 	def test_substitude(self):
 		s = "{dir}/{base}.{ext}"
 		filename = __file__
@@ -18,7 +20,20 @@ class TestSubstitude(TestCase):
 
 
 
-class TestCompiler(TestCase):
-
+class TestCompiler(unittest.TestCase):
 	def setUp(self):
-		pass		
+		self.comp = timus.Compiler.Compiler("dir {dir}", "{base}")
+
+	def test_compile(self):
+		fn = "file.ext"
+		self.assertEqual(self.comp.compile(fn), 0)
+		self.assertEqual(self.comp.bin_file_name(fn), abspath("file"))
+
+	def test_add_args(self):
+		self.comp.add_args("-wrong /opt")
+		self.assertEqual(self.comp.args, " -wrong /opt")
+		self.assertNotEqual(self.comp.compile("file.ext"), 0)
+
+
+if __name__ == '__main__':
+	unittest.main()
