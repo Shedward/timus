f7fbebac1992fad539ed76f1f1c63a6838b8430a
Maltsev Vlad <shedwardx@gmail.com>
2013-09-12 22:12:10 +0700

    Init testing.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 
-from setuptools import setup, find_packages
+from setuptools import setup
 from os.path import join, dirname
 
 setup(
@@ -8,11 +8,12 @@ setup(
 	author='Vlad Maltsev',
 	author_email='shedwardx@gmail.com',
 	url='https://github.com/Shedward/timus',
-	packages=["timus","timus.test"],
+	packages=["timus"],
 	description="Console client for acm.timus.ru with simple in/out test framework",
 	long_description=open(join(dirname(__file__), 'README.md')).read(),
 	license=open(join(dirname(__file__),"LICENSE")).read(),
 	scripts=['bin/timus'],
+	test_suite='timus.test',
 	install_requires=[
 		'psutil >= 0.6.1',
 		'PyYAML >= 3.10'
