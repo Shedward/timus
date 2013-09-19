3c2ce94ce3b02543fdd014685a0c5cde6e36792a
Maltsev Vlad <shedwardx@gmail.com>
2013-09-20 00:22:02 +0700

    Add tests for Timus.

---
+++ tests/Timus_test.py
@@ -0,0 +1,15 @@
+import unittest
+import pkg_resources
+
+from timus import Timus
+
+class TestTimus(unittest.TestCase):
+	def test_timus(self):
+		fn = pkg_resources.resource_filename('examples','example.cpp')
+		self.assertEqual(Timus.main(['test', fn, '--ll=vrb']), 0)
+
+	def test_timus_wrong_params(self):
+		self.assertRaises(Timus.WrongParams, 
+						  lambda : Timus.main([""]))
+		self.assertRaises(Timus.SourceFileNotFound, 
+				  		  lambda : Timus.main(["test", "no_file"]))
