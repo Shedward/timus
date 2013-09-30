9d3a903e8617e754d6bc5005b01420156bc4cfd7
Maltsev Vlad <shedwardx@gmail.com>
2013-10-01 00:46:43 +0700

    Version conflict fixing.

--- setup.py
+++ setup.py
@@ -17,8 +17,8 @@ setup(
 	install_requires=[
 		'psutil >= 0.6.1',
 		'PyYAML >= 3.10',
-		'requests >= 1.1.0-1',
-		'lxml >= 3.1.0-1',
+		'requests >= 1.1.0',
+		'lxml >= 3.1.0',
 		'appdirs >= 1.2.0'
 	],
 	include_package_data=True
