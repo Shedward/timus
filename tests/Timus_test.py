7092eb8c6954cb9acaf03bfd9b793ca4eb347f67
Maltsev Vlad <shedwardx@gmail.com>
2013-10-01 00:49:00 +0700

    Prevent compilation error with setdefs.

--- tests/Timus_test.py
+++ tests/Timus_test.py
@@ -4,8 +4,12 @@ import pkg_resources
 from timus import Timus
 from timus.RetCodes import RetCode
 from timus import Options
+from timus.Conf import Conf
 
 class TestTimus(unittest.TestCase):
+	def setUp(self):
+		Conf().reset()
+
 	def test_timus(self):
 		fn = pkg_resources.resource_filename('examples','example.cpp')
 		self.assertEqual(Timus.main(['test', fn, '-f']), RetCode.WrongOutput)
